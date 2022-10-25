537aa64fe3e23797eaa35fec2607d057b8b451e0
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2022-10-24 20:12:26 -0500

    7. HeroBanner y coneccion a Sanity

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import {Product, FooterBanner, HeroBanner} from '../components';
-const Home = () => {
-  return (
-    <>
-      
-      <HeroBanner/>
 
-      <div className='products-heading'>
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variations</p>
-      </div>
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
 
-      <div className='products-container'>
-          {['Product 1', 'Product 2'].map(
-            (product) => product
-          )}
-      </div>
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    {console.log(bannerData)}
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
+    </div>
 
-    
-      <FooterBanner/>
-    </>
-  )
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
+    </div>
+
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
+
+// Funcion getServerProps para hacer fetch
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
-export default Home
+export default Home;
